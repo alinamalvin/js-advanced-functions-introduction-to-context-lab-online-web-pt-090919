4e9a048dc576f2e471ff9d5c5d5a42a351705b98
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-16 22:39:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 function createEmployeeRecord([firstName, familyName, title, payRate]) {
-  "firstName": firstName, 
-    "familyName": familyName,
-    "title": title,
-    "payPerHour": payPerHour,
-    "timeInEvents": [],
-    "timeOutEvents": []
-  }
+  return employee.firstName
 }
